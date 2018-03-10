d73b3076c3ce980cf828a78620fd99656c9f74b4
imsnayak18 <36637636+imsnayak18@users.noreply.github.com>
2018-03-10 14:39:02 +0530

    Add files via upload

---
+++ tables_creation/insert_values_to_tables.sql
@@ -0,0 +1,24 @@
+insert into ser_cen values(1,'abc','hgc','yes',4764939,3468769,'Bangalore','11:00:00','21:00:00','1997');
+insert into ser_cen values(2,'adf','kbc','yes',4764939,3468769,'goa','08:00:00','17:00:00','2009');
+insert into ser_cen values(3,'dsf','dbc','no',4764939,3468769,'karwar','09:00:00','20:30:00','2014');
+
+insert into make_ser1 values(1,'amw');
+insert into make_ser1 values (1,'bmw');
+insert into make_ser1 values (1,'tata');
+insert into make_ser1 values (1,'atul');
+insert into make_ser1 values (1,'bajaj');
+insert into make_ser1 values (1,'force');
+insert into make_ser1 values(2,'amw');
+insert into make_ser1 values (2,'bmw');
+insert into make_ser1 values (2,'tata');
+insert into make_ser1 values(3,'hino');
+insert into make_ser1 values (3,'isuzu');
+insert into make_ser1 values (3,'lohia');
+
+insert into type_ser1 values (1,'cars');
+insert into type_ser1 values (1,'trucks');
+insert into type_ser1 values(2,' cars');
+insert into type_ser1 values (3,'cars');
+insert into type_ser1 values (3,'trucks');
+insert into type_ser1 values (3,'heavy_utility_vehicles');
+
